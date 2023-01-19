b108187994fc1028a9f41dbb8a652142e00f45c8
Ke Man Yi Nghi <ke.yinghi@student.vinci.be>
2023-01-20 00:59:40 +0100

    feat: adding test with table containing names with x

--- src/__tests__/checkNamesWithX.test.js
+++ src/__tests__/checkNamesWithX.test.js
@@ -4,8 +4,14 @@ const tableNames1 = ["Momo","Hadji","Leo"];
 const tableNames2 = ["DMX","Xzibit","JeanKevin"];
 const tableNames3 = ["JC","Leo","xena"];
 
+const tableRemovedNames2 = ["JeanKevin"];
+const tableRemovedNames3 = ["JC","Leo"];
+
 describe("test remove names with x function", () => {
     it("should return tableNames1", () => {
         expect(removeNamesWithX(tableNames1)).toEqual(tableNames1);
+    });
+    it("should return tableRemovedNames2", () => {
+        expect(removeNamesWithX(tableNames2)).toEqual(tableRemovedNames2);
     });
 });
